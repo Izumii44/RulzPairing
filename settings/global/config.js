bb3602c0e7ed3a19477b81f4bdc35c1b2987db42
KhrlMstfa <123540213+KhrlMstfa@users.noreply.github.com>
2023-10-23 07:13:08 +0000

    Initial commit

---
+++ settings/global/config.js
@@ -0,0 +1,18 @@
+/*Bukanya Gimana Tapi Kalo Mau No Enc Minimal Ya Buy Lah.
+Thanks To
+-God
+-Whiskeysockets
+-Nurutomo
+-Rull (me)
+-Zyy
+-Creator Module
+*/
+
+global.setBrowser = "Chrome (Linux)"
+//Dont Change Browser
+global.sessionName = "Auth-Session";
+
+/*
+Version: 1.0
+Kalo Eror Report Ke https://github.com/khrlmstfa/
+*/
